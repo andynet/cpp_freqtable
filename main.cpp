db3f6101ef3a8ce84eff58a348d2d1c4c902e8a8
andy <andrejbalaz001@gmail.com>
2021-05-18 17:21:55 +0200

    looks good, doesn't work

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cctype>
 #include <tuple>
 #include <map>
+#include <cstdlib>
 
 using namespace std;
 
@@ -86,55 +88,175 @@ void read_mutations(
 
 class sam_record {
 public:
-    string qname, rname, cigar, rnext, seq, qual;
-    int flag, pos, mapq, pnext, tlen;
+    // qname flag rname pos mapq cigar rnext pnext tlen seq qual tags
+    string qname, cigar, seq;
+    int pos = 0;
 
-    sam_record() {
-        flag = pos = mapq = pnext = tlen = 0;
-        qname = rname = cigar = rnext = seq = qual = "";
-    }
-
-    explicit sam_record(const string& line) : sam_record(){
+    explicit sam_record(const string& line){
         stringstream ss(line);
-        ss >> qname >> flag >> rname >> pos >> mapq >> cigar >> rnext >> pnext >> tlen >> seq >> qual;
+        string del;
+        ss >> qname >> del >> del >> pos >> del >> cigar >> del >> del >> del >> seq >> del;
     }
 };
+
+
+
+//void copy_sequence(const sam_record& current, record& record) {
+//    for (int i=0; i<current.seq.length(); i++) {
+//        record.seq[current.pos - 1 + i] = current.seq[i];
+//    }
+//}
+
+//string read_next_seq(fstream& aln, uint n){
+//    static string previous_qname;
+//    record record(n);
+//
+//    string line;
+//    while (!aln.eof()) {
+//        getline(aln, line);
+//        if (line.at(0) == '@') { continue; }    // skip header
+//        if (line.length() == 0) { continue; }   // skip empty lines
+//
+//        sam_record current = sam_record(line);
+//        if (previous_qname == current.qname) {
+//            copy_sequence(current, record);
+//        } else {
+//            // report
+//
+//
+//        }
+//
+//
+//        if (previous_qname != current.qname) {  // is this a bug?
+//            previous_qname = current.qname;
+//            return record.seq;
+//        }
+//
+//    }
+//    return "end";
+//}
 
 class record {
 public:
     string qname;   // query name, 1st column in the SAM file
-    int pos;        // position of the alignment, 4th column in the SAM file
     string seq;     // aligned sequence, 10th column, in the SAM file
+    string variant; // predicted using mut file
 
-    record() {qname=""; pos=0; seq="";}
+    record() {qname=""; seq=""; variant="";}
     explicit record(uint length) : record() {
         seq = string(length, 'N');
     }
 };
 
-string read_next_seq(fstream& aln, uint n){
-    static sam_record previous;
-    record record(n);
+int ctoi(char c) {
+    return (int)c - 48;
+}
 
-    string line;
-    while (!aln.eof()) {
-        getline(aln, line);
-        if (line[0] == '@') { continue; }
-        if (line.length() == 0) { continue; }
-        sam_record current = sam_record(line);
-        // cout << current.cigar << " ";
-        for (int i=0; i<current.seq.length(); i++) {
-            record.seq[current.pos - 1 + i] = current.seq[i];
-        }
-
-        if (previous.qname != current.qname) {
-            // cout << endl;
-            previous = current;
-            return record.seq;
+void parse_cigar(const string& cigar, vector<uint>& op_size, vector<char>& op_type) {
+    uint size = 0;
+    for (const char c : cigar) {
+        if (isdigit(c)) {
+            size = (size * 10) + ctoi(c);
+        } else {
+            op_size.push_back(size);
+            op_type.push_back(c);
+            size = 0;
         }
     }
-    return "";
 }
+
+template <typename T>
+void print_vector(const vector<T>& vec, const string& sep=", ") {
+    for (auto e : vec) {
+        cout << e << sep;
+    }
+    cout << endl;
+}
+
+void match(const string& source, uint& pos, uint size, string& destination) {
+    for (int i=0; i<size; i++) {
+        destination[pos + i] = source[i];
+    }
+    pos += size;
+}
+
+void deletion(uint& pos, uint size) {
+    pos -= size;
+}
+
+void insertion(uint& pos, uint size) {
+    pos += size;
+}
+
+void add_seq(const sam_record& sam_rec, record& rec) {
+    vector<uint> op_size;
+    vector<char> op_type;
+    parse_cigar(sam_rec.cigar, op_size, op_type);
+    // print_vector(op_type);
+    // print_vector(op_size);
+
+    uint i = sam_rec.pos - 1;
+    for (int j = 0; j < op_type.size(); j++) {
+        if (op_type[j] == 'M') {
+            match(sam_rec.seq, i, op_size[j], rec.seq);
+        } else if (op_type[j] == 'D') {
+            // deletion(i, op_size[j]);
+        } else if (op_type[j] == 'I') {
+            // insertion(i, op_size[j]);
+        } else if (op_type[j] == 'H') { // intentionally do nothing
+        } else if (op_type[j] == 'S') { // intentionally do nothing
+        } else {
+            cout << "Unexpected operation type " << op_type[j] << ". Allowed values are {M, D, I, H, S}.";
+        }
+    }
+}
+
+class sam_reader {
+private:
+    fstream sam;
+    string current_line;
+    string current_qname;
+
+public:
+    explicit sam_reader(const string& aln_file) {
+        sam = fstream(aln_file, fstream::in);
+    }
+
+    bool eof() {
+        return sam.eof();
+    }
+
+    void skip_header() {
+        getline(sam, current_line);
+        while (current_line[0] == '@') {
+            getline(sam, current_line);
+        }
+        stringstream ss(current_line);
+        ss >> current_qname;
+    }
+
+    void print_current_line() {
+        cout << current_line << endl;
+    }
+
+    record get_next_record(uint n) {
+        record rec(n);
+        string new_line;
+        string new_qname;
+        do {
+            sam_record sam_rec(current_line);
+            rec.qname = sam_rec.qname;
+            add_seq(sam_rec, rec);
+            getline(sam, new_line);
+            stringstream ss(new_line);
+            ss >> new_qname;
+            current_line = new_line;
+        } while (current_qname == new_qname);
+        // current_line = new_line;
+        current_qname = new_qname;
+        return rec;
+    }
+};
 
 tuple<char, uint, char> parse_snp(string s) {
     uint n = s.length();
@@ -161,6 +283,13 @@ int main() {
     // outputs:
     string tsv_filename = "/home/andy/projects/cpp_freqtable/data/full_out.tsv";
 
+//    // inputs:
+//    string ref_filename = "/home/andy/projects/cpp_freqtable/data/ref.fa";
+//    string aln_filename = "/home/andy/projects/cpp_freqtable/data/example_aln.sam";
+//    string mut_filename = "/home/andy/projects/cpp_freqtable/data/mut.txt";
+//    // outputs:
+//    string tsv_filename = "/home/andy/projects/cpp_freqtable/data/full_out.tsv";
+
     // read reference
     string ref = read_reference(ref_filename);
     cout << ref << endl;
@@ -182,13 +311,25 @@ int main() {
     //      assign variant
     //      add counts to fullout
 
-    fstream aln(aln_filename, fstream::in);
-    while (!aln.eof()) {
-        string seq = read_next_seq(aln, ref.length());
-        string variant = get_seq_variant(seq, snp);
-        cout << seq << variant << endl;
+//    fstream aln(aln_filename, fstream::in);
+//    while (!aln.eof()) {
+//
+//         string seq = read_next_seq(aln, ref.length());
+//         cout << seq << endl;
+//        // string variant = get_seq_variant(seq, snp);
+//        // cout << seq << variant << endl;
+//
+//    }
 
+    sam_reader sr(aln_filename);
+    sr.skip_header();
+    sr.print_current_line();
+    while (!sr.eof()) {
+        record rec = sr.get_next_record(ref.length());
+        cout << rec.qname << endl << rec.seq << endl;
     }
+    record rec = sr.get_next_record(ref.length());
+    cout << rec.qname << endl << rec.seq << endl;
 
     // store fullout
     delete_3d_array(A, ref.length(), variants.size(), alphabet.size());
